f68c30f61da4938c737eb79bef9269ad68a36c3a
ArtTheClown-007 <himendersharma18@gmail.com>
2024-12-20 09:11:23 +0530

    formatted the doc

--- 1.c
+++ 1.c
@@ -2,36 +2,28 @@
 
 // Problems from exercise F
 
-int problemA(){
+int problemA()
+{
 
-/*Ramesh’s basic salary is input through the keyboard. His dearness 
-allowance is 40% of basic salary, and house rent allowance is 20% of 
-basic salary. Write a program to calculate his gross salary. */
+    /*Ramesh’s basic salary is input through the keyboard. His dearness
+    allowance is 40% of basic salary, and house rent allowance is 20% of
+    basic salary. Write a program to calculate his gross salary. */
 
+    double salary;
+    double da = 0.4, hra = 0.2, gs;
 
-double salary;
-double da=0.4, hra=0.2,gs;
+    printf("Enter Ramesh's basic salary: ");
+    scanf("%lf", &salary);
 
-printf("Enter Ramesh's basic salary: ");
-scanf("%lf",&salary);
+    gs = salary + (salary * da) + (salary * hra);
 
-gs = salary + (salary*da) + (salary*hra);
+    printf("Gross salary of Ramesh is: %.2lf", gs);
 
-printf("Gross salary of Ramesh is: %.2lf",gs);
-
-return 0;
-
-
-
+    return 0;
 }
 
+int main()
+{
 
-int main(){
-
-problemA();
-
-
-
+    problemA();
 }
-
-
