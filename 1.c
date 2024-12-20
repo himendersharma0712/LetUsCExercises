c8f1d37173d63a1758571c9ec2870b951859dd9c
ArtTheClown-007 <himendersharma18@gmail.com>
2024-12-20 09:21:52 +0530

    added prog B

--- 1.c
+++ 1.c
@@ -22,8 +22,39 @@ int problemA()
     return 0;
 }
 
+
+void problemB(){
+    /*The distance between two cities (in km.) is input through the 
+keyboard. Write a program to convert and print this distance in 
+meters, feet, inches and centimeters. */
+
+double distance;
+double meter,feet,inches,cm;
+
+printf("Enter the distance betwixt two cities(in km): ");
+scanf("%lf",&distance);
+
+// 1 feet = 30.48cm = 0.3048m
+// 1 inch = 2.54cm
+
+meter = distance*1000;
+feet =  (distance*1000)/0.3048;
+inches = (distance*1000*100)/2.54;
+cm = distance*1000*100;
+
+printf("The distance in meter is: %.3lf\n",meter);
+printf("The distance in feet is: %.2lf\n",feet);
+printf("The distance in inches is: %.2lf\n",inches);
+printf("The distance in centimeters is: %.2lf\n",cm);
+
+
+
+}
+
 int main()
 {
 
     problemA();
+
+    problemB();
 }
