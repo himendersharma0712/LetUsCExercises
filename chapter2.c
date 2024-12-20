dfd85d8051ea1c3aeb335218d0f0ec56a1e95df2
ArtTheClown-007 <himendersharma18@gmail.com>
2024-12-20 13:54:35 +0530

    minor tweaks

--- chapter2.c
+++ chapter2.c
@@ -209,7 +209,7 @@ printf("\nC is %d and D is %d.",C,D);
 
 
 
-problemI(){
+void problemI(){
 /*Consider a currency system in which there are notes of seven 
 denominations, namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If 
 a sum of Rs. N is entered through the keyboard, write a program to 
@@ -265,5 +265,7 @@ int main(){
 // problemG();
 
 // problemH();
+
+problemI();
 
 }
