ac99367a10cce2686f3477f326b19584c4cebd2b
ArtTheClown-007 <himendersharma18@gmail.com>
2024-12-21 15:38:43 +0530

    added ptr to func

---
+++ chapter22.c
@@ -0,0 +1,20 @@
+/*Using Ptr to functions*********************************/
+
+
+// #include <stdio.h>
+
+// int (*ptrFunc)();
+
+// int sum(int a, int b){
+//   return a + b;
+// }
+
+// int main(){
+//   ptrFunc = sum;
+//   printf("%d\n",ptrFunc(1,2));
+// }
+
+/**********************************************************/
+
+
+
